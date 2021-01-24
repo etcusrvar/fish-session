77fd8d951d1bb1d3b50e7bb8c701fd928d9300f2
etcusrvar <etcusrvar@users.noreply.github.com>
2021-01-24 10:26:51 -0600

    fix: was unintentionally setting fish_private_mode during check

--- conf.d/session.fish
+++ conf.d/session.fish
@@ -1,5 +1,10 @@
-if set fish_private_mode 1 &>/dev/null
+if set -q fish_private_mode
+    if set fish_private_mode $fish_private_mode &>/dev/null
+        set -g __session_fish_private_mode_settable 1
+    else
+        set -g __session_fish_private_mode 1
+    end
+else if set -l fish_private_mode 1 &>/dev/null
     set -g __session_fish_private_mode_settable 1
-else if set -q fish_private_mode
-    set -g __session_fish_private_mode 1
+    set -e fish_private_mode
 end
